9afd4081e0a7b891328da040795827f770a3ec8d
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-07-24 13:33:43 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule } from '@nestjs/microservices/module';
+import { Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'COMMUNICATION',
+        transport: Transport.TCP,
+      },
+      {
+        name: 'ANALYTICS',
+        transport: Transport.TCP,
+        options: { port: 3001 },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
